e70c44f469b69ad6742ee486ef9cf02afc01cb21
Kannar <lucas.valentin.isart@gmail.com>
2014-03-28 15:21:16 +0100

    Début levelManager

--- sources/config/levels_data.js
+++
@@ -1,8 +0,0 @@
-/*************************
-*   Données générales
-*   des niveaux
-*************************/
-var generalsLevel3_data = 
-{
-    tiles: "tileset3"
-};
---
+++ sources/functions/levelManager.js
@@ -0,0 +1,22 @@
+//Changement de level
+function getNextLevel(currentLevel)
+{
+
+}
+
+//Données des levels
+var allLevels_data = [
+    {
+        level : 1,
+        tiledDatas : level1_part1_data,
+        tilesetsData : tiles1_data,
+        specialTiles : specialTiles1
+    },
+    {
+        level : 2,
+        tiledDatas : level1_part2_data,
+        tilesetsData : tiles1_data,
+        specialTiles : specialTiles1
+    }
+
+];
